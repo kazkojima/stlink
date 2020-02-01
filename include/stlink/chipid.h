014dd39846069dcfe4759c36fd374b9545fca68e
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2020-02-02 08:49:35 +0900

    Add Option Byte Write command for STM32G031.

--- include/stlink/chipid.h
+++ include/stlink/chipid.h
@@ -72,6 +72,7 @@ enum stlink_stm32_chipids {
 	STLINK_CHIPID_STM32_F413             = 0x463,
 	STLINK_CHIPID_STM32_L4RX             = 0x470, // taken from the STM32L4R9I-DISCO board
 	STLINK_CHIPID_STM32_G0X1             = 0x460,
+	STLINK_CHIPID_STM32_G031             = 0x466,
 	STLINK_CHIPID_STM32_WB55             = 0x495
 };
 
--- src/chipid.c
+++ src/chipid.c
@@ -533,6 +533,17 @@ static const struct stlink_chipid_params devices[] = {
             .bootrom_size = 0x7800           // 30K (table 2)
         },
         {
+            // STM32G031 (from RM0444)
+            .chip_id = STLINK_CHIPID_STM32_G031,
+            .description = "G031 device",
+            .flash_type = STLINK_FLASH_TYPE_G0,
+            .flash_size_reg = 0x1FFF75E0,    // Section 38.2
+            .flash_pagesize = 0x800,         // 2K (sec 3.2)
+            .sram_size = 0x2000,             // 8K (sec 2.3)
+            .bootrom_base = 0x1fff0000,
+            .bootrom_size = 0x7800           // 30K (table 2)
+        },
+        {
             // STM32WB55 (from RM0434)
             .chip_id = STLINK_CHIPID_STM32_WB55,
             .description = "WB55 device",
